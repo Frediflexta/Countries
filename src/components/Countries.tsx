183ca4ec22bd1f169a32936c879d816c8eaf3401
Frediflexta <fredrick.adegoke@gmail.com>
2024-02-05 13:17:41 +0100

    chore: mocked an api result for a country to build out a component

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,6 +1,17 @@
+import Country from "./Country";
+import data from "../data.json";
+
 const Countries = () => {
   return (
-    <section className="pt-20">body containing the flags and details</section>
+    <section className="pl-20 pt-14">
+      <Country
+        name={data[190].name}
+        image={data[190].flags.svg}
+        region={data[190].region}
+        capital={data[190].capital}
+        population={data[190].population}
+      />
+    </section>
   );
 };
 
--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,3 +1,41 @@
-const Country = () => {};
+type CountryProps = {
+  name: string;
+  image: string;
+  population: number;
+  region: string;
+  capital: string;
+};
+
+const Country = ({
+  name,
+  image,
+  population,
+  region,
+  capital,
+}: CountryProps) => {
+  return (
+    <div className="h-[336px] w-[264px] rounded-md bg-white text-[#111517] shadow-md">
+      <img className="max-h-40 w-full" src={image} alt="" />
+      <p className="pl-6 pt-6 text-lg font-extrabold">{name}</p>
+      <div className="pl-6 pt-3 text-sm">
+        <p>
+          <span className="bold_span">Population:</span>
+          <span className="extra_thin_span">
+            {" "}
+            {population.toLocaleString()}
+          </span>
+        </p>
+        <p>
+          <span className="bold_span">Region:</span>{" "}
+          <span className="extra_thin_span">{region}</span>
+        </p>
+        <p>
+          <span className="bold_span">Capital:</span>{" "}
+          <span className="extra_thin_span">{capital}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Country;
