9c64df7dab06c17ec06b34f3a00e5c5dcb17931b
Frediflexta <fredrick.adegoke@gmail.com>
2024-03-31 09:48:39 +0100

    chore: fix deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   root: "src",
   build: {
-    outDir: "dist", // Change 'public' to your desired output directory name
+    outDir: "dist",
   },
 });
